8ed8c09e3761918cf2322f8e395a0f42e1d6252b
Chidiebere John <chidieberejohn17@gmail.com>
2022-09-09 12:39:16 +0100

    On branch master

--- Js/main.js
+++ Js/main.js
@@ -5,3 +5,21 @@ toggles.forEach(toggle => {
     toggle.parentNode.classList.toggle('active');
   });
 });
+
+const hamburgerIcon = document.querySelector('.hamburger__wrapper')
+const menuBar = document.querySelector('.mobile-nav__container')
+const menuBarOverlay = document.querySelector('#overlay')
+
+hamburgerIcon.onclick = showMenuBar
+
+function showMenuBar() {
+    hamburgerIcon.classList.toggle('active')
+    menuBar.classList.toggle('slide-out')
+    menuBarOverlay.classList.toggle('active')
+}
+
+menuBarOverlay.addEventListener('click', function() {
+    hamburgerIcon.classList.remove('active')
+    menuBar.classList.remove('slide-out')
+    menuBarOverlay.classList.remove('active')
+})
