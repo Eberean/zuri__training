60a22d612b447679e7e8a2ec4858f553e0a3703e
Chidiebere John <chidieberejohn17@gmail.com>
2022-09-21 00:19:58 +0100

    Changes to be committed:

--- Js/signup.js
+++ Js/signup.js
@@ -1,5 +1,10 @@
+const firstName = document.querySelector('#firstname');
+const lastName = document.querySelector('#lastname');
+const Email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const eyeIcon = document.querySelector('#eyeIcon');
+const errorMsg = document.querySelectorAll('.error')
+const form = document.querySelector('#form');
 
 eyeIcon.addEventListener('click', (e) => {
   e.preventDefault();
@@ -10,5 +15,32 @@ eyeIcon.addEventListener('click', (e) => {
   else {
     password.setAttribute('type', 'password');
     eyeIcon.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(firstName.value.trim() === '') {
+    errorMsg[0].innerHTML = 'First Name cannot be empty'
+  }else {
+    errorMsg[0].innerHTML = ''
+  }
+
+  if(lastName.value.trim() === '') {
+    errorMsg[1].innerHTML = 'Last Name cannot be empty'
+  }else {
+    errorMsg[1].innerHTML = ''
+  }
+
+  if(Email.value.trim() === '') {
+    errorMsg[2].innerHTML = 'Provide a valid email address'
+  }else {
+    errorMsg[2].innerHTML = ''
+  }
+
+  if(password.value.trim() === '') {
+    errorMsg[3].innerHTML = 'Password cannot be Blank'
+  }else {
+    errorMsg[3].innerHTML = ''
   }
 })
